2dddc9d592a4db4ba78870cf08e3616222e4e1d7
koushiga <koushigakannan99@gmail.com>
2023-10-17 16:33:34 -0700

    update

---
+++ script.js
@@ -0,0 +1,135 @@
+
+let con=document.getElementById("cont");
+
+let tab1=document.createElement("div");
+tab1.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table1(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab1.innerText=sum;
+	};
+}
+table1(1,15);
+
+let tab2=document.createElement("div");
+tab2.setAttribute("class","col_1");
+	for(let i=1;i<=15;i++){
+		function table2(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab2.innerText=sum;
+	};
+}
+table2(2,15);
+
+let tab3=document.createElement("div");
+tab3.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table3(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab3.innerText=sum;
+	};
+}
+table3(3,15);
+
+let tab4=document.createElement("div");
+tab4.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table4(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab4.innerText=sum;
+	};
+}
+table4(4,15);
+
+let tab5=document.createElement("div");
+tab5.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table5(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab5.innerText=sum;
+	};
+}
+table5(5,15);
+
+let tab6=document.createElement("div");
+tab6.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table6(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab6.innerText=sum;
+	};
+}
+table6(6,15);
+
+let tab7=document.createElement("div");
+tab7.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table7(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab7.innerText=sum;
+	};
+}
+table7(7,15);
+
+let tab8=document.createElement("div");
+tab8.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table8(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab8.innerText=sum;
+	};
+}
+table8(8,15);
+
+let tab9=document.createElement("div");
+tab9.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+		function table9(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab9.innerText=sum;
+	};
+}
+table9(9,15);
+
+let tab10=document.createElement("div");
+tab10.setAttribute("class","col_1");
+for(let i=1;i<=15;i++){
+	function table10(a,b){
+		let sum="";
+		for(let j=1;j<=15;j++){
+		sum+=a+"*"+j+"="+(a*j)+"\n";
+		}
+		tab10.innerText=sum;
+	};
+}
+table10(10,15);
+
+con.append(tab1,tab2,tab3,tab4,tab5,tab6,tab7,tab8,tab9,tab10);
+
